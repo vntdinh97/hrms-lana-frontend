11cd8ab87656e2d0f3903dfc40f8c1ceb3567bd4
Dinh Nguyen Thien Vo <dinhvnt.evnit@evn.com.vn>
2021-11-10 18:17:30 +0700

    a

---
+++ src/app/components/excel-export/delete-work/delete-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-work',
+  templateUrl: './delete-work.component.html',
+  styleUrls: ['./delete-work.component.scss']
+})
+export class DeleteWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
